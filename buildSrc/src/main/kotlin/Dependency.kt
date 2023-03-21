ab3b6acbfd5858349cc3ab7d80bdc0d067177a0c
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-03-21 16:09:33 +0900

    build: Config & Dependency Setting

---
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,35 @@
+object Dependency {
+
+    // jvm
+    const val KTOR_CORE: String = "io.ktor:ktor-server-core-jvm:${Version.KTOR}"
+    const val KTOR_NETTY: String = "io.ktor:ktor-server-netty-jvm:${Version.KTOR}"
+
+    // serialization
+    const val KTOR_SERIALIZATION: String = "io.ktor:ktor-serialization:${Version.KTOR}"
+
+    // auth
+    const val KTOR_AUTH: String = "io.ktor:ktor-server-auth-jvm:${Version.KTOR}"
+    const val KTOR_AUTH_JWT: String = "io.ktor:ktor-server-auth-jwt-jvm:${Version.KTOR}"
+    const val KTOR_CORS: String = "io.ktor:ktor-server-cors-jvm:${Version.KTOR}"
+
+    // logging
+    const val KTOR_LOGGING: String ="io.ktor:ktor-server-call-logging-jvm:${Version.KTOR}"
+    const val LOGBACK: String = "ch.qos.logback:logback-classic:${Version.LOGBACK}"
+
+    // test
+    const val KTOR_TEST: String = "io.ktor:ktor-server-tests-jvm:${Version.KTOR}"
+    const val KOTLIN_TEST: String = "org.jetbrains.kotlin:kotlin-test-junit:${Version.KOTLIN}"
+
+    // database
+    const val EXPOSED_CORE: String = "org.jetbrains.exposed:exposed-core:${Version.EXPOSED}"
+    const val EXPOSED_DAO: String = "org.jetbrains.exposed:exposed-dao:${Version.EXPOSED}"
+    const val EXPOSED_JDBC: String = "org.jetbrains.exposed:exposed-jdbc:${Version.EXPOSED}"
+    const val EXPOSED_TIME: String = "org.jetbrains.exposed:exposed-java-time:${Version.EXPOSED}"
+    const val MYSQL: String = "com.mysql:mysql-connector-j:${Version.MYSQL}"
+    const val HIKARI: String = "com.zaxxer:HikariCP:${Version.HIKARI}"
+
+    // koin
+    const val KOIN_LOGGER: String = "io.insert-koin:koin-logger-slf4j:${Version.KOIN}"
+    const val KOIN: String = "io.insert-koin:koin-ktor:${Version.KOIN}"
+
+}
---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,9 @@
+object Version {
+    const val LOGBACK = "1.4.5"
+    const val KTOR = "2.2.4"
+    const val KOTLIN = "1.8.10"
+    const val MYSQL = "8.0.32"
+    const val EXPOSED = "0.41.1"
+    const val KOIN = "3.3.1"
+    const val HIKARI = "5.0.1"
+}
