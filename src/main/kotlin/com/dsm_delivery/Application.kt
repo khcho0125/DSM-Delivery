231d2660252651988d0ebab9d5f8335c07f58a2f
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-03-21 16:09:52 +0900

    build: Application

---
+++ src/main/kotlin/com/dsm_delivery/Application.kt
@@ -0,0 +1,22 @@
+package com.dsm_delivery
+
+
+import com.dsm_delivery.plugins.DataBaseFactory
+import com.dsm_delivery.plugins.configureHTTP
+import com.dsm_delivery.plugins.configureMonitoring
+import com.dsm_delivery.plugins.configureRouting
+import com.dsm_delivery.plugins.configureSecurity
+import com.dsm_delivery.plugins.injectModule
+import io.ktor.server.application.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+fun Application.module() {
+    DataBaseFactory(environment.config)
+    injectModule()
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureRouting()
+}
