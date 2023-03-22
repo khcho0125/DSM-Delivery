40c0612be1eda0c4b19c10ba69da3c6d8dcb7b2c
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-03-22 19:58:08 +0900

    add: (#1) Student QueryFactory

---
+++ src/main/kotlin/com/dsm_delivery/persistence/factory/StudentQuery.kt
@@ -0,0 +1,64 @@
+package com.dsm_delivery.persistence.factory
+
+import com.dsm_delivery.persistence.entity.Student
+import com.dsm_delivery.persistence.entity.StudentTable
+import com.dsm_delivery.persistence.repository.StudentRepository
+import com.dsm_delivery.plugins.DataBaseFactory
+import com.dsm_delivery.plugins.DataBaseFactory.dbQuery
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.exists
+import org.jetbrains.exposed.sql.select
+import java.util.UUID
+
+/**
+ *
+ * 학생에 관한 질의를 요청하는 StudentQuery
+ *
+ * @author Chokyunghyeon
+ * @date 2023/03/22
+ **/
+class StudentQueryFactory : StudentRepository {
+
+    private fun toEntity(row: ResultRow): Student = Student(
+        id = row[StudentTable.id].value,
+        name = row[StudentTable.name],
+        number = row[StudentTable.number],
+        password = row[StudentTable.password],
+        sex = row[StudentTable.sex]
+    )
+
+    override suspend fun findById(id: UUID): Student? = dbQuery {
+        StudentTable
+            .select { StudentTable.id eq id }
+            .singleOrNull()
+            ?.let(::toEntity)
+    }
+
+    override suspend fun findByNumber(number: Int): Student? = dbQuery {
+        StudentTable
+            .select { StudentTable.number eq number }
+            .singleOrNull()
+            ?.let(::toEntity)
+    }
+
+    override suspend fun findByName(name: String): Student? = dbQuery {
+        StudentTable
+            .select { StudentTable.name eq name }
+            .singleOrNull()
+            ?.let(::toEntity)
+    }
+
+    override suspend fun findBy(where: () -> Op<Boolean>): Student? = dbQuery {
+        StudentTable
+            .select(where())
+            .singleOrNull()
+            ?.let(::toEntity)
+    }
+
+    override suspend fun existsById(id: UUID): Boolean = dbQuery {
+        StudentTable
+            .select { StudentTable.id eq id }
+            .empty()
+    }
+}
---
+++ src/main/kotlin/com/dsm_delivery/persistence/repository/StudentRepository.kt
@@ -0,0 +1,24 @@
+package com.dsm_delivery.persistence.repository
+
+import com.dsm_delivery.persistence.entity.Student
+import org.jetbrains.exposed.sql.Op
+import java.util.UUID
+
+/**
+ *
+ * 학생을 관리하는 StudentRepository
+ *
+ * @author Chokyunghyeon
+ * @date 2023/03/22
+ **/
+interface StudentRepository {
+    suspend fun findById(id: UUID): Student?
+
+    suspend fun findByNumber(number: Int): Student?
+
+    suspend fun findByName(name: String): Student?
+
+    suspend fun findBy(where: () -> Op<Boolean>): Student?
+
+    suspend fun existsById(id: UUID): Boolean
+}
