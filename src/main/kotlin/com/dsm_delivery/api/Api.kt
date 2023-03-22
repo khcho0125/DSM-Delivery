c007b1d2a32106269693993cf22d0eb7741d695d
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-03-22 09:50:23 +0900

    docs: (#1) Add Comment

--- src/main/kotlin/com/dsm_delivery/api/Api.kt
+++ src/main/kotlin/com/dsm_delivery/api/Api.kt
@@ -3,6 +3,13 @@ package com.dsm_delivery.api
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
+/**
+ *
+ * Routing을 도와주는 Api
+ *
+ * @author Chokyunghyeon
+ * @date 2023/03/22
+ **/
 abstract class Api(private val route: Routing.() -> Unit) {
     operator fun invoke(app: Application) = app.routing(route)
 }
