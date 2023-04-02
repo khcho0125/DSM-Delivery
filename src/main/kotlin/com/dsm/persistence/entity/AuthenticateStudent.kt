f0c5150a888567c8d27d5deed25765813e034de1
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-02 20:21:47 +0900

    refactor: (#5) Incorrect Usage - Table

--- src/main/kotlin/com/dsm/persistence/entity/AuthenticateStudent.kt
+++ src/main/kotlin/com/dsm/persistence/entity/AuthenticateStudent.kt
@@ -1,7 +1,6 @@
 package com.dsm.persistence.entity
 
 import com.dsm.exception.StudentExceptions
-import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
@@ -13,10 +12,12 @@ import org.jetbrains.exposed.sql.Table
  * @date 2023/03/31
  **/
 object AuthenticateStudentTable : Table("tbl_authenticate_student") {
-    val number: Column<EntityID<Int>> = integer("number").entityId()
+    val number: Column<Int> = integer("number")
     val name: Column<String> = varchar("name", Student.NAME_MAX_LENGTH)
     val sex: Column<Sex> = enumerationByName("sex", Sex.VALUE_MAX_LENGTH, Sex::class)
     val isUsed: Column<Boolean> = bool("is_used")
+
+    override val primaryKey: PrimaryKey = PrimaryKey(number)
 }
 
 data class AuthenticateStudent(
@@ -28,7 +29,7 @@ data class AuthenticateStudent(
 
     fun used(): AuthenticateStudent = copy(isUsed = true)
 
-    fun check(name: String) {
+    operator fun invoke(name: String) {
         when {
             isUsed -> throw StudentExceptions.Conflict(ALREADY_USED)
             this.name != name -> throw StudentExceptions.Unauthorized(DIFFERENT_NAME)
