73c6ffd00c05248bc5fb1cee475e790cb8daf7bc
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-02 20:14:04 +0900

    refactor: (#5) Change Class Name TokenContainer → TokenResult

--- src/main/kotlin/com/dsm/domain/auth/token/GenerateToken.kt
+++ src/main/kotlin/com/dsm/domain/auth/token/GenerateToken.kt
@@ -3,20 +3,24 @@ package com.dsm.domain.auth.token
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.dsm.plugins.SecurityProperties
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 import java.util.Date
 import java.util.UUID
 
 /**
  *
- * Jwt 토큰을 생성하는 GenerateJwt
+ * Jwt 토큰을 생성하는 GenerateToken
  *
  * @author Chokyunghyeon
  * @date 2023/03/20
  **/
-data class TokenContainer(
+@Serializable
+data class TokenResult(
     val accessToken: String,
     val refreshToken: String,
+    @Contextual
     val accessTokenExpired: LocalDateTime
 )
 
@@ -45,8 +49,8 @@ class JwtGenerator(
             .sign(Algorithm.HMAC256(securityProperties.secret))
     }
 
-    override suspend fun generateToken(studentId: UUID): TokenContainer {
-        return TokenContainer(
+    override suspend fun generateToken(studentId: UUID): TokenResult {
+        return TokenResult(
             accessToken = generateAccessToken(studentId),
             refreshToken = generateRefreshToken(),
             accessTokenExpired = LocalDateTime.now().plusSeconds(securityProperties.accessExpired)
