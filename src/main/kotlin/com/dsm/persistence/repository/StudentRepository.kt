91891a68fd1dce3a07216ba2aabd3d8ca1473687
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-27 00:10:24 +0900

    revert: (#14) Remove Basic FindBy Method

--- src/main/kotlin/com/dsm/persistence/repository/StudentRepository.kt
+++ src/main/kotlin/com/dsm/persistence/repository/StudentRepository.kt
@@ -1,7 +1,6 @@
 package com.dsm.persistence.repository
 
 import com.dsm.persistence.entity.Student
-import org.jetbrains.exposed.sql.Op
 import java.util.UUID
 
 /**
@@ -18,11 +17,7 @@ interface StudentRepository {
 
     suspend fun findByName(name: String): Student?
 
-    suspend fun findBy(where: () -> Op<Boolean>): Student?
-
     suspend fun existsById(id: UUID): Boolean
-
-    suspend fun existsBy(where: () -> Op<Boolean>): Boolean
 
     suspend fun insert(student: Student): Student
 }
