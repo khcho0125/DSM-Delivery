36207d607483ce551f350425f54a25501eb98a7e
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-04-27 00:13:41 +0900

    add: (#14) Get Current StudentId Function

--- src/main/kotlin/com/dsm/plugins/Security.kt
+++ src/main/kotlin/com/dsm/plugins/Security.kt
@@ -3,13 +3,16 @@ package com.dsm.plugins
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.dsm.domain.auth.token.JwtGenerator
+import com.dsm.exception.DomainException
 import com.dsm.exception.RefreshTokenException
 import com.dsm.persistence.repository.StudentRepository
 import com.dsm.plugins.database.dbQuery
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.auth.authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.auth.principal
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.response.respond
 import org.koin.ktor.ext.getKoin
@@ -64,6 +67,14 @@ fun Application.configureSecurity() {
             }
         }
     }
+}
+
+fun ApplicationCall.currentUserId() : UUID {
+    val principal: JWTPrincipal = principal<JWTPrincipal>()
+        ?: throw DomainException.InternalServerError("JWT Principal Undefined")
+
+    return principal.payload.getClaim(JwtGenerator.JWT_STUDENT_ID).asString()
+        .let(UUID::fromString)
 }
 
 object SecurityProperties {
