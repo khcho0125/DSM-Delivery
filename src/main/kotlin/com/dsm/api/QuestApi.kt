d6e0a30f20a3f1753189ba1bd770c3d42fbff2e0
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-05-03 22:50:04 +0900

    add: (#24) Get Quest Function

--- src/main/kotlin/com/dsm/api/QuestApi.kt
+++ src/main/kotlin/com/dsm/api/QuestApi.kt
@@ -1,10 +1,13 @@
 package com.dsm.api
 
-import com.dsm.domain.mission.usecase.PostMission
+import com.dsm.domain.mission.usecase.GetQuest
+import com.dsm.domain.mission.usecase.PostQuest
 import com.dsm.plugins.currentUserId
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.patch
@@ -17,41 +20,48 @@ import org.koin.dsl.module
 
 /**
  *
- * 배달 미션의 API를 관리하는 MissionApi
+ * 배달 퀘스트의 API를 관리하는 QuestApi
  *
  * @author Chokyunghyeon
  * @date 2023/04/26
  **/
-class MissionApi(
-    postMission: PostMission
+class QuestApi(
+    postQuest: PostQuest,
+    getQuest: GetQuest
 ) : Api({
     route("/mission") {
         get {
-
+            call.respond(
+                message = getQuest(),
+                status = HttpStatusCode.OK
+            )
         }
+    }
 
+    authenticate("/quest") {
         post {
-            val request: PostMission.Request = call.receive()
+            val request: PostQuest.Request = call.receive()
             val studentId: Int = call.currentUserId()
 
-            postMission(request, studentId)
+            postQuest(request, studentId)
 
             call.response.status(HttpStatusCode.NoContent)
         }
 
-        patch {
+        patch("/{quest-id}") {
 
         }
 
-        delete {
+        delete("/{quest-id}") {
 
         }
     }
 }) {
     companion object {
         val module: Module = module {
-            singleOf(::PostMission)
-            singleOf(::MissionApi) bind Api:: class
+            singleOf(::PostQuest)
+            singleOf(::GetQuest)
+            singleOf(::QuestApi) bind Api:: class
         }
     }
 }
