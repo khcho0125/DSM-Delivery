87c600efa7288fb588b8d64b6a518d1dd03b19c1
Jo Kyung Hyeon <khcho0125@icloud.com>
2023-05-16 14:34:55 +0900

    Modify Quest Exception

--- src/main/kotlin/com/dsm/exception/QuestException.kt
+++ src/main/kotlin/com/dsm/exception/QuestException.kt
@@ -1,5 +1,7 @@
 package com.dsm.exception
 
+import com.dsm.persistence.entity.QuestState
+
 /**
  *
  * 퀘스트에 관한 예외를 발생시키는 QuestException
@@ -18,8 +20,13 @@ object QuestException {
     class NotFound(override val message: String? = null)
         : DomainException.NotFound(message, QuestErrorCode.NOT_FOUND)
 
-    class NotPublishing(override val message: String? = null)
-        : DomainException.Conflict(message, QuestErrorCode.NOT_PUBLISHING)
+    class DifferentState(
+        override val message: String? = null
+    ) : DomainException.Conflict(message, QuestErrorCode.DIFFERENT_STATE) {
+        constructor(state: QuestState) : this(
+            message = "Not ${state.name} Quest State"
+        )
+    }
 
     class UnableAccept(override val message: String? = null)
         : DomainException.Conflict(message, QuestErrorCode.UNABLE_ACCEPT)
@@ -33,8 +40,8 @@ enum class QuestErrorCode(
     NOT_FOUND(1, "Quest Not Found"),
     ALREADY_POSTED(2, "The other quest has been posted"),
     OUT_OF_LIMIT_LENGTH(3, "The length is too much long"),
-    NOT_PUBLISHING(4, "Not Publishing Quest"),
-    UNABLE_ACCEPT(5, "Can't Accepting Quest")
+    DIFFERENT_STATE(4, "A Different Quest State"),
+    UNABLE_ACCEPT(5, "Can't Accepting Quest"),
 
     ;
 
